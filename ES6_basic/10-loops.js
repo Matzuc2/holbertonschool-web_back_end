3a26612ab4d406981d13ae7141584c60ce1bb5d5
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-03-25 09:38:51 +0100

    refactor: clean up console.log statements and improve code formatting

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-    let result = [];
-    for (let idx of array) { 
-      result.push(appendString + array[idx])
-    }
-    return result;
+  const result = [];
+  for (const idx of array) {
+    result.push(appendString + array[idx]);
   }
+  return result;
+}
