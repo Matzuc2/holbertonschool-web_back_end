36c2a14bd1d9b2636e9040a38ecdc86b527d94a0
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-03-26 13:05:07 +0100

    fix: remove unnecessary newline in Building class constructor

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,7 +1,6 @@
 export default class Building {
   constructor(sqft) {
     if (typeof this.evacuationWarningMessage !== 'function') throw new Error('Class extending Building must override evacuationWarningMessage');
-
     this._sqft = sqft;
   }
 
