953b0829c3a97f7187f6e8add0f54531c153912e
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-03-26 14:32:07 +0100

    style: format Car and HolbertonClass files for consistency and readability

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,14 +1,16 @@
-export default class Car{
-    constructor(brand, motor, color){
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-    }
-    static get [Symbol.species]() {
-        return this;
-      }
-    cloneCar(){
-        const Car = this.constructor[Symbol.species];
-        return new Car;
-    }
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Car = this.constructor[Symbol.species];
+    return new Car();
+  }
 }
