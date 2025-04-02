d058fca71c6fe4397e28148d9908c44e26912300
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-04-02 14:53:08 +0200

    fix: add return statement to Promise.all in handleProfileSignup function

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   const prom1 = uploadPhoto();
   const prom2 = createUser();
 
-  Promise.all([prom1, prom2]).then((values) => {
+  return Promise.all([prom1, prom2]).then((values) => {
     console.log(`${values[0].body}, ${values[1].firstName}, ${values[1].lastName}`);
   })
     .catch(() => {
