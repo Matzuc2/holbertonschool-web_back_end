0aa1ddbc89980d0177a7fc6d42842047fe7190c3
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-04-02 14:52:14 +0200

    refactor: improve string interpolation in handleProfileSignup function

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup() {
   const prom2 = createUser();
 
   Promise.all([prom1, prom2]).then((values) => {
-    console.log(values[0].body, values[1].firstName, values[1].lastName);
+    console.log(`${values[0].body}, ${values[1].firstName}, ${values[1].lastName}`);
   })
     .catch(() => {
       console.error('Signup system offline');
