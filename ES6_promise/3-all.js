330afc79434142906200bda2a1888a32f8b3865f
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-04-02 15:15:14 +0200

    refactor: streamline promise handling in handleProfileSignup function

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,13 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const prom1 = uploadPhoto();
-  const prom2 = createUser();
-
-  Promise.all([prom1, prom2]).then(([photo, user]) => {
-    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-  })
-    .catch(() => {
-      console.log('Signup system offline');
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch((error) => {
+      console.error('Signup system offline:', error);
     });
 }
