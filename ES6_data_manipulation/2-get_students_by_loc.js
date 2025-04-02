4ce994272c332c3d6e65c2f197c06349bda5b84a
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-04-02 14:29:08 +0200

    refactor: simplify getStudentsByLocation function by removing input validation

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,4 @@
 export default function getStudentsByLocation(array, city) {
-  if (Array.isArray(array) && typeof city === 'string') {
-    const filterList = array.filter((x) => x.location === city);
-    return filterList;
-  }
+  const filterList = array.filter((x) => x.location === city);
   return filterList;
 }
