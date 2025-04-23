cebb40f4820086ea426c43684c4cf487ab9015ab
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-04-23 14:36:56 +0200

    feat: add floor function to compute the floor of a floating-point number

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to compute the floor of a floating-point number.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Compute the floor of a floating-point number.
+
+    Args:
+        n (float): The floating-point number to floor.
+
+    Returns:
+        int: The largest integer less than or equal to the given number.
+    """
+    return int(n)
