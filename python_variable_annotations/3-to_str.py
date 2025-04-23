b8ef4a93dd21d9a2623649a723e95f8c9d7f6bac
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-04-23 14:40:58 +0200

    feat: add to_str module to convert floating-point numbers to strings

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to convert a floating-point number to a string.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a floating-point number to a string.
+
+    Args:
+        n (float): The floating-point number to convert.
+
+    Returns:
+        str: The string representation of the given number.
+    """
+    return str(n)
