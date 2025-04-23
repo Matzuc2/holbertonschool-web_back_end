8e10aa4e5a696adc9df30b9cf8d84d877d8e1251
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-04-23 17:08:23 +0200

    feat: add variable definitions and main module for demonstration

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+This module defines and annotates several variables with specific types.
+"""
+
+a: int = 1
+"""
+a (int): An integer variable initialized to 1.
+"""
+
+pi: float = 3.14
+"""
+pi (float): A floating-point variable representing the value of pi.
+"""
+
+i_understand_annotations: bool = True
+"""
+i_understand_annotations (bool): A boolean variable indicating
+"""
+
+school: str = "Holberton"
+"""
+school (str): A string variable representing the name of the school.
+"""
