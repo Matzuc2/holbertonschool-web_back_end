d8e009d45e72ede98b9be610b7dc728badaac360
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-04-23 17:22:57 +0200

    feat: add sum_list module to compute the sum of a list of floating-point numbers

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to compute the sum of a list of float.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Compute the sum of a list of floating-point numbers.
+
+    Args:
+        input_list (List[float]): A list of floating-point numbers.
+
+    Returns:
+        float: The sum of the numbers in the list.
+    """
+    return sum(input_list)
