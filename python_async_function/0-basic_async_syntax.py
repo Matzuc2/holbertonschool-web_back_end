20e26c88b6506292623f4b9d55ecf19c31456a41
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-04-24 12:17:41 +0200

    fix: change max_delay type from float to int in wait_random function

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: float = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     Asynchronous coroutine that waits for random delay between 0 and max_delay
     (inclusive) seconds and returns the delay value.
