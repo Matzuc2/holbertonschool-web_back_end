ebea8707396f112409f225c1f161cd9e019bc83d
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-04-24 12:16:00 +0200

    fix: remove unnecessary docstring details in wait_random function

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """
 This module contains an asynchronous coroutine that waits for a random delay
-and returns the delay value.
 """
-
 import random
 import asyncio
 
@@ -12,12 +10,6 @@ async def wait_random(max_delay: float = 10) -> float:
     """
     Asynchronous coroutine that waits for random delay between 0 and max_delay
     (inclusive) seconds and returns the delay value.
-
-    Args:
-        max_delay (float): The maximum delay in seconds. Defaults to 10.
-
-    Returns:
-        float: The actual delay value.
     """
     random_number = random.uniform(0, max_delay)
     await asyncio.sleep(random_number)
