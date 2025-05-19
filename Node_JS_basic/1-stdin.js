74676a2296ff0f2a90fa6a9a209d2fe186e307c5
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-05-19 23:22:32 +0200

    chore: initialize Node.js project with package.json and dependencies

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+});
+console.log('Welcome to Holberton School, what is your name?');
+rl.on('line', (line) => {
+  if (process.stdin.isTTY) {
+    console.log(`Your name is: ${line}`);
+    process.exit(0);
+  } else {
+    console.log(`Your name is: ${line}`);
+    console.log('This important software is now closing');
+  }
+});
