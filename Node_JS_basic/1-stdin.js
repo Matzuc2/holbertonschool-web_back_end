4a869daa2364815cd39e1ca68c641e427c38858e
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-05-20 09:39:07 +0200

    fix: add newline to closing message in stdin prompt

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -10,6 +10,6 @@ rl.on('line', (line) => {
     process.exit(0);
   } else {
     console.log(`Your name is: ${line}`);
-    console.log('This important software is now closing');
+    console.log('This important software is now closing\n');
   }
 });
