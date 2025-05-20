2b0d902d8e2325b1460f37d04371494756565a3a
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-05-20 14:20:51 +0200

    feat: add initial Express server setup with a basic route

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,10 @@
+// Import Express
+const express = require('express');
+
+const app = express();
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+// Start the server
+app.listen(1245);
