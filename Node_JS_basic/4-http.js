a7256987977cfd50c659d5831722ce7c3d71707b
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-05-20 10:40:17 +0200

    fix: correct HTTP server implementation and response handling

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,14 +1,10 @@
-// Étape 1: Importer le module HTTP
-import http from 'http';
+const http = require('http');
 
-// Étape 2: Créer le serveur
 const app = http.createServer((req, res) => {
-  // À chaque requête vers le serveur, ce code sera exécuté
-
-  res.write('Hello Holberton School!'); // On définit le statut HTTP et les en-têtes de la réponse
-  res.end(); // On envoie une réponse au client
+// À chaque requête vers le serveur, ce code sera exécuté
+  res.writeHead(200, { 'Content-Type': 'text/plain' }); // On définit le statut HTTP et les en-têtes de la réponse
+  res.end('Hello Holberton School!'); // On envoie une réponse au client
 });
-
-// Étape 3: Démarrer le serveur
+// Étape 3: Démarrer le serve
 app.listen(1245);
 module.exports = app;
