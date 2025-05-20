e136b2f5606b09cf5f4ffa01a604f61d2000aa74
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-05-21 01:29:08 +0200

    fix: replace console.log with process.stdout.write for consistent output handling

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,14 +4,16 @@ const rl = readline.createInterface({
   input: process.stdin,
 });
 process.stdin.setEncoding('utf8');
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?');
 rl.on('line', (line) => {
   if (process.stdin.isTTY) {
-    console.log(`Your name is: ${line}`);
+    if (line){
+      (process.stdout.write(`Your name is: ${line}`));
+    }
     rl.close();
   } else {
-    console.log(`Your name is: ${line}`);
-    console.log('This important software is now closing');
+    process.stdout.write(`Your name is: ${line}`);
+    process.stdout.write('This important software is now closing');
     rl.close();
   }
 });
