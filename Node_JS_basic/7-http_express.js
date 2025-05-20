b0ad4b1653e0228c961bf729c29b61b234238e5d
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-05-20 15:26:32 +0200

    feat: implement Express server with student listing route and update express dependency

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,17 @@
+// Import Express
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2])
+    .then((data) => {
+      res.send(`This is the list of our students\n${data}`);
+    });
+});
+// Start the server
+app.listen(1245);
