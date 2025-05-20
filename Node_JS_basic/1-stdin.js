bc7119b23d33cb0fbedd42fcaff368de1c7638ff
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-05-21 01:24:53 +0200

    fix: add missing semicolon for consistency in stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,11 +3,12 @@ const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
 });
+process.stdin.setEncoding('utf8');
 console.log('Welcome to Holberton School, what is your name?');
 rl.on('line', (line) => {
   if (process.stdin.isTTY) {
     console.log(`Your name is: ${line}`);
-    rl.close()
+    rl.close();
   } else {
     console.log(`Your name is: ${line}`);
     console.log('This important software is now closing');
