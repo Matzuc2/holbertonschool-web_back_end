0ba7856419128abf7880341982c358afb4995785
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-05-20 10:33:43 +0200

    fix: correct indentation and formatting in HTTP server code

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,12 +3,12 @@ import http from 'http';
 
 // Étape 2: Créer le serveur
 const app = http.createServer((req, res) => {
-    // À chaque requête vers le serveur, ce code sera exécuté
+  // À chaque requête vers le serveur, ce code sera exécuté
 
-    res.write('Hello Holberton School!'); // On définit le statut HTTP et les en-têtes de la réponse
-    res.end(); // On envoie une réponse au client
+  res.write('Hello Holberton School!'); // On définit le statut HTTP et les en-têtes de la réponse
+  res.end(); // On envoie une réponse au client
 });
 
 // Étape 3: Démarrer le serveur
-app.listen(1245)
+app.listen(1245);
 module.exports = app;
