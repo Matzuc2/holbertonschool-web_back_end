470f6496c661f0a52dda54ded3fb9e48551604b3
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-05-20 10:31:51 +0200

    fix: update HTTP response handling and remove console log on server start

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -5,12 +5,10 @@ import http from 'http';
 const app = http.createServer((req, res) => {
     // À chaque requête vers le serveur, ce code sera exécuté
 
-    res.writeHead(200, { 'Content-Type': 'text/plain' }); // On définit le statut HTTP et les en-têtes de la réponse
-    res.end('Hello Holberton School!'); // On envoie une réponse au client
+    res.write('Hello Holberton School!'); // On définit le statut HTTP et les en-têtes de la réponse
+    res.end(); // On envoie une réponse au client
 });
 
 // Étape 3: Démarrer le serveur
-app.listen(1245, () => {
-    console.log('Serveur démarré sur http://localhost:1245');
-});
+app.listen(1245)
 module.exports = app;
