c1d8ed011e0e154c19b04ec50c7a8ddd6e43daff
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-05-21 01:30:48 +0200

    fix: add missing newlines for consistent output formatting

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,16 +4,16 @@ const rl = readline.createInterface({
   input: process.stdin,
 });
 process.stdin.setEncoding('utf8');
-process.stdout.write('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 rl.on('line', (line) => {
   if (process.stdin.isTTY) {
-    if (line){
-      (process.stdout.write(`Your name is: ${line}`));
+    if (line) {
+      (process.stdout.write(`Your name is: ${line}\n`));
     }
     rl.close();
   } else {
-    process.stdout.write(`Your name is: ${line}`);
-    process.stdout.write('This important software is now closing');
+    process.stdout.write(`Your name is: ${line}\n`);
+    process.stdout.write('This important software is now closing\n');
     rl.close();
   }
 });
