d0188b8c1842866a9c3496dc421a62fe4a94eeed
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-05-20 13:11:19 +0200

    fix: correct formatting and indentation in HTTP server code

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,25 +2,26 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-// À chaque requête vers le serveur, ce code sera exécuté
+  // À chaque requête vers le serveur, ce code sera exécuté
 
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('Hello Holberton School!'); // On envoie une réponse au clien
-  }
-  else if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!'); // On envoie une réponse au client
+  } else if (req.url === '/students') {
     res.writeHead(200, { 'Content-Type': 'text/plain' }); // On définit le statut HTTP et les en-têtes de la réponse
-    res.write("This is the list of our students\n")
+    res.write('This is the list of our students\n');
     countStudents('./database.csv')
-    .then((data) =>{
-        res.end(data)
-    })
-  }
-  else{
+      .then((data) => {
+        res.end(data);
+      })
+      .catch((error) => {
+        res.end(`Error: ${error.message}`);
+      });
+  } else {
     res.statusCode = 404;
     res.end('Not found');
   }
 });
-// Étape 3: Démarrer le serve
+// Étape 3: Démarrer le serveur
 app.listen(1245);
 module.exports = app;
