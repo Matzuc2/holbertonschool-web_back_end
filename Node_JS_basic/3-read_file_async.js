41d6857e1cd40f570c14c7ac2061b8dc9e061e97
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-05-20 10:00:23 +0200

    refactor: update countStudents function to read CSV data directly from file

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,14 +1,36 @@
-const fs = require('fs');
-const csv = require('csv-parser');
-const getStream = require('get-stream');
+const fs = require('fs').promises;
 
 async function countStudents(path) {
-readCSVData = async (path) => {
-  const parseStream = csv({delimiter: ','});
-  const data = await getStream.array(fs.createReadStream(path).pipe(parseStream))
-  return data
-};
-    data = await readCSVData(path)
-    //continue tomorrow..
+  try {
+    const data = await fs.readFile(path, 'utf8');
+
+    const rows = data.split('\n').slice(1);
+    let namelistCS = [];
+    let namelistSWE = [];
+    let CountCS = 0;
+    let CountSWE = 0;
+    const CountAll = rows.length;
+
+    rows.forEach((row) => {
+      const columns = row.split(',');
+      if (columns[3] === 'CS') {
+        namelistCS.push(String(columns[0]));
+        CountCS += 1;
+      } else if (columns[3] === 'SWE') {
+        namelistSWE.push(String(columns[0]));
+        CountSWE += 1;
+      }
+    });
+
+    namelistCS = namelistCS.toString().replace(/,/g, ', ');
+    namelistSWE = namelistSWE.toString().replace(/,/g, ', ');
+
+    console.log(`Number of students: ${CountAll}`);
+    console.log(`Number of students in CS: ${CountCS}. List: ${namelistCS}`);
+    console.log(`Number of students in SWE: ${CountSWE}. List: ${namelistSWE}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
+
 module.exports = countStudents;
