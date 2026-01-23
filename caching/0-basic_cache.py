14a159cdd3a497d3a8bbcf5eb79ada8947f247b2
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-01-23 15:52:38 +0100

    feat: implement BasicCache and BaseCaching classes with put and get methods

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from base_caching import BaseCaching
+class BasicCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+    def put(self, key, item):
+        if key and item :
+            self.cache_data[key] = item
+    def get(self, key):
+        if self.cache_data.get(key):
+            value = self.cache_data.get(key)
+            return value
+        else:
+            return None
