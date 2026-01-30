365659488b8a46a02b4bea39aa6385080225d533
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-01-30 11:05:17 +0100

    adding  new tasks

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,41 @@
+from base_caching import BaseCaching
+
+class LRUCache(BaseCaching):
+    def __init__(self):
+        """Initialize the LIFOCache instance by calling parent constructor."""
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item to the cache using LIFO eviction policy.
+
+        Args:
+            key: The key under which to store the item.
+            item: The item to store in the cache.
+
+        If either key or item is None, this method does nothing.
+        If the cache exceeds MAX_ITEMS, the last item added is removed.
+        """
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            a = list(self.cache_data)[0]
+            self.cache_data.pop(a)
+            print(f"DISCARD: {a}")
+
+    def get(self, key):
+        """Retrieve an item from the cache by key.
+
+        Args:
+            key: The key of the item to retrieve.
+
+        Returns:
+            The value associated with the key, or None if the key doesn't
+            exist or is None.
+        """
+        if key and self.cache_data.get(key):
+            item = self.cache_data.get(key)
+            self.cache_data.pop(key)
+            self.cache_data[key] = item
+            return self.cache_data.get(key)
+        else:
+            return None
