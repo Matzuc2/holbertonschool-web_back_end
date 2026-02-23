cf579819a21ea59a9cc2183a2bfd854beea58991
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-02-23 16:43:26 +0100

    fix: add return type hint for get_db function

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -127,11 +127,11 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     grant connexion to database with env variables
     """
-    cnx = mysql.connector.connect(
+    cnx: mysql.connector.connection.MySQLConnection = mysql.connector.connect(
             user=os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
             password=os.getenv("PERSONAL_DATA_DB_PASSWORD", "root"),
             database=os.getenv("PERSONAL_DATA_DB_NAME", "localhost")
