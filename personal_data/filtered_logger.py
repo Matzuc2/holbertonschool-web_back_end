6189c5831237c66646683e34dd6483bac8ef59d0
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-02-23 16:12:37 +0100

    fix: update PII_FIELDS to include 'ssn' for improved data redaction

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -13,7 +13,7 @@ import logging
 import typing
 from logging import Logger
 
-PII_FIELDS = ("password", "phone", "ip", "name", "email")
+PII_FIELDS = ("ssn", "phone", "ip", "name", "email")
 def filter_datum(fields: typing.List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
