bcb453b03f0bc301555a987d2cdf9841b2b34b0e
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-03-06 14:11:26 +0100

    feat: add session authentication support in app and create SessionAuth class

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -11,6 +11,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.
 
 
 app = Flask(__name__)
@@ -24,6 +25,8 @@ if auth == "auth":
 elif auth == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif auth == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
 else:
     from api.v1.auth.auth import Auth
     auth = Auth()
---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from auth import Auth
+
+class SessionAuth(Auth):
+    pass
