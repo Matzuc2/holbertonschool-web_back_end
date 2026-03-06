e29b6b7f84e5d6559625f13e69afcc5b24c07a41
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-03-06 15:23:55 +0100

    feat: implement session creation logic in SessionAuth class

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 """Session authentication module for the API."""
-
 from api.v1.auth.auth import Auth
+from uuid import uuid4
+import uuid
 
 
 class SessionAuth(Auth):
     """Session-based authentication class."""
 
-    pass
+    user_id_by_session_id: dict = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if not user_id:
+            return None
+        if type(user_id) is not str:
+            return None
+        else:
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
