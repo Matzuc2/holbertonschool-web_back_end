f3da271e772f13d9f2a28a68065ed58e816d9f82
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-03-20 17:23:11 +0100

    fix: correct error response codes and format JSON response in login function

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -18,9 +18,9 @@ def login():
         return jsonify({'error': 'password missing'}), 400
     users = User.search({'email': email})
     if not users and len(users) == 0:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email" }), 404
     if not users[0].is_valid_password(password):
-        return jsonify({"error": "wrong password"}),404 
+        return jsonify({"error": "wrong password"}), 401 
     from api.v1.app import auth
     import os
     session_id = auth.create_session(users[0].id)
