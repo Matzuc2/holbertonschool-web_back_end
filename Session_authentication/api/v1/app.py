19e3a2c86cc807f397f043e07bf0c52ebc31adb8
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-03-20 14:28:06 +0100

    fix: update before_request to check for unauthorized access after verifying current_user

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -105,10 +105,10 @@ def before_request():
         '/api/v1/auth_session/login/'
         ]
     current_user = auth.current_user(request)
-    if not auth.require_auth(request.path, paths_without_auth):
-        return
     if not current_user:
         abort(403)
+    if not auth.require_auth(request.path, paths_without_auth):
+        return
     if not auth.authorization_header(request) and not auth.session_cookie(request):
         abort(401)
     if not auth.authorization_header(request):
