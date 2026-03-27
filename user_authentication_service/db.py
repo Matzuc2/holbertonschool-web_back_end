8d2c489c7957878160a5a1709d76165f34048b92
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-03-27 13:31:39 +0100

    fix: remove unnecessary blank line in DB class

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -28,7 +28,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email, hashed_password):
         """Add a new user in database session object
         """
