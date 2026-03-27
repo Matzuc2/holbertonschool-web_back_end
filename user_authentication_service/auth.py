8ee4fdd188dd82ea1b942751308786aef95b3e11
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-03-27 16:28:29 +0100

    fix: correct indentation in _hash_password function for consistency

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,10 +3,10 @@ import bcrypt
 
 
 def _hash_password(password) -> bytes:
-        """hash user password and returns it
-        """
+    """hash user password and returns it
+    """
 
-        bytes = password.encode('utf-8')
-        salt = bcrypt.gensalt()
-        hashed_pwd = bcrypt.hashpw(password=bytes, salt=salt)
-        return hashed_pwd
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_pwd = bcrypt.hashpw(password=bytes, salt=salt)
+    return hashed_pwd
