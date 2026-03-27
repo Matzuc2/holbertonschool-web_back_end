1b1d32935c504470534fcd12bc240273ef75a8b4
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-03-27 16:40:42 +0100

    feat: implement Auth class for user registration and password hashing

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import bcrypt
+from user import User
+from db import DB
 
 
 def _hash_password(password) -> bytes:
@@ -10,3 +12,20 @@ def _hash_password(password) -> bytes:
     salt = bcrypt.gensalt()
     hashed_pwd = bcrypt.hashpw(password=bytes, salt=salt)
     return hashed_pwd
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email, password):
+        user = self._db.find_user_by(email=email)
+        if user:
+            raise ValueError(f"User {user.email} already exists.")
+        else:
+            hashed_pwd = _hash_password(password)
+            self._db.add_user(email=email, hashed_password=hashed_pwd)
+            return User
