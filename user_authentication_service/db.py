a8ad5628008c88807c56b98406a4b87358814a77
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-03-27 14:36:54 +0100

    feat: implement user retrieval method and add main execution file

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 from user import Base, User
 
 
@@ -37,3 +37,12 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs):
+        """ Find an user by his id on the session obj
+        """
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        else:
+            return user
