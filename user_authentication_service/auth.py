a87392ca26677a19982ad861a9b219e580e1fe1b
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-03-27 16:27:41 +0100

    refactor: move _hash_password function out of Auth class for better modularity

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,11 +2,10 @@
 import bcrypt
 
 
-class Auth():
-    def _hash_password(self, password) -> bytes:
+def _hash_password(password) -> bytes:
         """hash user password and returns it
         """
-        
+
         bytes = password.encode('utf-8')
         salt = bcrypt.gensalt()
         hashed_pwd = bcrypt.hashpw(password=bytes, salt=salt)
