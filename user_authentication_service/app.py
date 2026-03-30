07f8af39b1a4efdcdae5cfb99ae81349328f2039
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-03-30 21:20:39 +0200

    fix: update hello_world function to return JSON response

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+#!/usr/bin/env python3
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-        return {
-        "message": "Bienvenue" 
-    }
+    return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
