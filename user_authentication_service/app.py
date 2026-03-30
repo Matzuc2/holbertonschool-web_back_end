edefb31f0fb9a57e53b897dadff27ed250f8ed32
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-03-30 22:14:30 +0200

    feat: implement user registration endpoint with error handling

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
 def hello_world():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['GET', 'POST'])
+def register():
+    email = request.form.get('email')
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email=email, password=password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
