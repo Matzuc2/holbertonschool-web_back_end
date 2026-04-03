c23abddaa97cd4a20f281ed82a4685beb5009c6d
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-04-03 17:11:47 +0200

    fix: improve error handling in get_reset_password_token method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -83,10 +83,13 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """reset the password token of the user by his email"""
-        user = self._db.find_user_by(email=email)
-        if not user:
+        try:
+            user = self._db.find_user_by(email=email)
+            if not user:
+                raise ValueError
+            else:
+                new_token = _generate_uuid()
+                self._db.update_user(user.id, reset_token=new_token)
+                return new_token
+        except NoResultFound:
             raise ValueError
-        else:
-            new_token = _generate_uuid()
-            self._db.update_user(user.id, reset_token=new_token)
-            return new_token
