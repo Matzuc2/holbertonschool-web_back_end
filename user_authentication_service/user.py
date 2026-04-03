aa7470d4200871decc9304cf9aa8ec63b60387e8
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-04-03 14:37:37 +0200

    feat: add docstrings to User model for improved documentation

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+"""User module for database models."""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
@@ -7,6 +9,15 @@ Base = declarative_base()
 
 
 class User(Base):
+    """User model for SQLAlchemy ORM.
+
+    Attributes:
+        id: User ID (primary key).
+        email: User email address.
+        hashed_password: Hashed user password.
+        session_id: Current session ID.
+        reset_token: Password reset token.
+    """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
