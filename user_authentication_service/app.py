5adc57c97013fa4b9bf942ddad7239f616a99c17
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-04-10 09:53:15 +0200

    feat: add reset password token generation endpoint

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -61,5 +61,16 @@ def profile():
         return jsonify({"email": user.email}), 200
 
 
+@app.route("/reset_password", methods=['POST'])
+def get_reset_password_token():
+    email = request.form.get('email')
+    try:
+        user = AUTH._db.find_user_by(email=email)
+        new_token = AUTH.get_reset_password_token(user.email)
+        return jsonify({"email": email, "reset_token": new_token}), 200
+    except NoResultFound:
+        return make_response("", 403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
