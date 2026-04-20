7d514375ef24a4d2183e08aaa8fc5a6df9cd73fb
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-04-20 15:49:52 +0200

    test: add unit tests for get_json function

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 import unittest
 from parameterized import parameterized, parameterized_class
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -22,6 +23,21 @@ class TestAccessNestedMap(unittest.TestCase):
         """test the error is as expected with raise in access_nested_map"""
         with self.assertRaises(KeyError, msg=path):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, expected, mock_get):
+        """test with mocked data"""
+        mock_get.return_value = Mock()
+        mock_get.return_value.json.return_value = expected
+        result = get_json(test_url)
+        mock_get.assert_called_once(test_url)
+        self.assertEqual(expected, result)
 
 
 if __name__ == '__main__':
