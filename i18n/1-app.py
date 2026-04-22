759b0154ef9cd09d0919c07442cec4133880c570
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-04-22 11:55:07 +0200

    style: remove unused import and improve docstring for Config class

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -2,11 +2,12 @@
 """Basic flask app"""
 
 
-from flask import Flask, render_template
+from flask import Flask
 from flask_babel import Babel
 
 
 class Config():
+    """Config class with class variables for i18n"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = LANGUAGES[0]
     BABEL_DEFAULT_TIMEZONE = 'UTC'
