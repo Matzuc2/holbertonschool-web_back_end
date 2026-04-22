7892f4e9ac7bcfece5f4f854350b3d461688e669
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-04-22 12:43:42 +0200

    feat: implement basic Flask app with internationalization support

---
+++ i18n/2-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Basic flask app"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config():
+    """Config class with class variables for i18n"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def index():
+    """return basic html template"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
