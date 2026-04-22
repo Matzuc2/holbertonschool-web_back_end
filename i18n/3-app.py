852d128186ec134abacff353adc73b7d036953ed
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-04-22 16:46:03 +0200

    fix: update gettext function for consistency and improve docstring clarity in 3-app.py

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,35 +1,40 @@
 #!/usr/bin/env python3
 """Basic flask app"""
 
-
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext as _
+from flask_babel import Babel, gettext as babel_gettext
 
 
 class Config():
-    """Config class with class variables for i18n"""
+    """Config class with class variables for i18n."""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = LANGUAGES[0]
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 def get_locale():
-    """get locale language from config dick"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """Select the best language match from the request."""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+def gettext(message):
+    """Translate a message using Flask-Babel."""
+    return babel_gettext(message)
 
 
 app = Flask(__name__)
-babel = Babel(app)
 app.config.from_object(Config)
+babel = Babel(app)
 babel.init_app(app, locale_selector=get_locale)
-_("home_title")
-_("home_header")
+
+gettext("home_title")
+gettext("home_header")
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    """return basic html template"""
-    return render_template('1-index.html')
+    """Return basic html template."""
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
