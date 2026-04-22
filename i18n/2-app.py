52bdcfad6f4387e2fe45bbb9cf04b5fd75e57721
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-04-22 15:43:28 +0200

    style: add blank lines for improved readability in 2-app.py

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -12,8 +12,10 @@ class Config():
     BABEL_DEFAULT_LOCALE = LANGUAGES[0]
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 app = Flask(__name__)
 babel = Babel(app)
