aba53aaa20c1b42c1b4b17336c8574b5f4fe0fd4
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-04-22 11:39:57 +0200

    feat: add initial Flask app with basic configuration and language support

---
+++ i18n/1-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Basic flask app"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config():
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+app.config['BABEL_DEFAULT_LOCALE'] = Config.LANGUAGES[0]
+app.config['BABEL_DEFAULT_TIMEZONE'] = Config.
+babel = Babel(app)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
