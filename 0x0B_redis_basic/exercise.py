d2328c3569af55f9d9c5846f880608e4ab2e3acc
Matzuc2 <9793@holbertonstudents.com>
2026-05-22 12:41:37 +0200

    fix: update store method to use uuid4 for key generation and improve type hinting

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
+from typing import Union
 import redis
 import uuid
 
 
-class Cache():
+class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: str | bytes | int | float) -> str:
-        key = str(uuid.uuid1())
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
