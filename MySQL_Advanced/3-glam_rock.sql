32ae28f6069c5784797c161850a12e0ddc2e9c81
Matzuc2 <9793@holbertonstudents.com>
2026-06-05 14:21:45 +0200

    fix: update SQL query comment for clarity and adjust condition to use LIKE for Glam rock style

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
--- Select the band name, lifespan where certain condition
+-- Select the band name and lifespan of bands with Glam rock style
 SELECT band_name, (COALESCE(split, 2024) - formed) AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
