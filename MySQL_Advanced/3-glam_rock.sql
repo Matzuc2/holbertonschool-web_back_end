2fc95c1205047e5a2b23d9e870d6040afb4eb33b
Matzuc2 <9793@holbertonstudents.com>
2026-06-05 14:15:57 +0200

    fix: update lifespan calculation to handle NULL values and refine query conditions for glam rock bands

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
 -- Select the band name, lifespan where certain condition
-SELECT band_name, (split - formed) AS lifespan
+SELECT band_name, (COALESCE(split, 2024) - formed) AS lifespan
 FROM metal_bands
-WHERE split <= 2024
-GROUP BY band_name, lifespan
+WHERE style = 'Glam rock'
 ORDER BY lifespan DESC
