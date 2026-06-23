95af50e7fface4359e85870c87c2012a999f1d20
Matzuc2 <9793@holbertonstudents.com>
2026-06-23 17:18:45 +0200

    Add express as a dependency in package.json

---
+++ unittests_in_js/8-api/api.test.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  it('Correct status code?', (done) => {
+    request.get('http://localhost:7865', (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Correct result?', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('Other?', (done) => {
+    request.get('http://localhost:7865', (error, response) => {
+      expect(response).to.exist;
+      done();
+    });
+  });
+});
