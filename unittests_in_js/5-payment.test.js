226969ae64760eec93a4ce9347f7915e7c52ed3a
Matzuc2 <9793@holbertonstudents.com>
2026-06-23 13:39:17 +0200

    feat: add sendPaymentRequestToApi function and corresponding tests

---
+++ unittests_in_js/5-payment.test.js
@@ -0,0 +1,28 @@
+
+const assert = require('assert');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+const calculateNumber = require('./2-calcul_chai');
+const { beforeEach, afterEach } = require('mocha');
+
+let spy; 
+
+describe('sendPaymentRequestToApi', function() {
+    beforeEach(() =>{
+        spy = sinon.spy(console, 'log');
+    })
+    it('should call sendRequestToAPI with 100, 20', function() {
+        sendPaymentRequestToApi(100, 20);
+        assert(spy.calledWith('The total is: 120'));
+        assert(spy.calledOnce())
+    });
+    it('should call sendRequestToAPI with 10, 10', function() {
+        sendPaymentRequestToApi(10, 10);
+        assert(spy.calledWith('The total is: 20'));
+        assert(spy.calledOnce())
+    });
+    afterEach(() => {
+        spy.restore();
+    })
+});
