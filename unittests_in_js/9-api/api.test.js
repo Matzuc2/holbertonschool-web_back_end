e0bc551db9b34cf71db72781355fd786ded1c624
Matzuc2 <9793@holbertonstudents.com>
2026-06-23 18:11:20 +0200

    feat: add API and corresponding tests for payment system and cart functionality

---
+++ unittests_in_js/9-api/api.test.js
@@ -0,0 +1,48 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  it('Correct status code?', (done) => {
+    request.get('http://localhost:7865', (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Correct result?', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('Other?', (done) => {
+    request.get('http://localhost:7865', (error, response) => {
+      expect(response).to.exist;
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  it('Correct status code when id is a number?', (done) => {
+    request.get('http://localhost:7865/cart/12', (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Correct result when id is a number?', (done) => {
+    request.get('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('Correct status code when id is NOT a number?', (done) => {
+    request.get('http://localhost:7865/cart/hello', (error, response) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
